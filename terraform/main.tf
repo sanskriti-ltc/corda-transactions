198026ca3c4df4d79bd6d592289f2e55ce333f8f
sanskriti-ltc <sanskriti.singh@lloydsbanking.com>
2025-03-18 13:12:51 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,102 +1,32 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
-}
-
-data "google_client_config" "default" {}
-
 resource "google_container_cluster" "primary" {
-  name     = "corda-cluster"
-  location = var.region
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  networking_mode = "VPC_NATIVE"
-
-  master_auth {
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
-  location   = var.region
-  cluster    = google_container_cluster.primary.name
-  node_count = 1
+  provider           = google
+  name               = "gke-cluster"
+  location           = "us-central1-a"
+  initial_node_count = 4  # Increase initial node count to 4
 
   node_config {
-    machine_type = "e2-standard-4"
-    disk_size_gb = 50
-    preemptible  = true
+    machine_type = "e2-medium"
+  }
 
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+  network_policy {
+    enabled = true
   }
 }
 
-resource "kubernetes_service" "corda_service" {
-  metadata {
-    name = "corda-service"
+resource "google_container_node_pool" "primary_node_pool" {
+  provider            = google
+  cluster             = google_container_cluster.primary.name
+  location           = google_container_cluster.primary.location
+  name                = "default-pool"
+  initial_node_count  = 4  # Start with 4 nodes
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 6
+    target_cpu_utilization = 0.75  # Target average CPU utilization for scaling
   }
 
-  spec {
-    selector = {
-      app = "corda"
-    }
-
-    port {
-      port        = 8888
-      target_port = 8888
-    }
-
-    type = "LoadBalancer"
-  }
-}
-
-resource "kubernetes_deployment" "corda" {
-  metadata {
-    name = "corda-node"
-    labels = {
-      app = "corda"
-    }
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        app = "corda"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "corda"
-        }
-      }
-
-      spec {
-        container {
-          name  = "corda"
-          image = "corda/corda-enterprise:latest"
-
-          port {
-            container_port = 8888
-          }
-        }
-      }
-    }
+  node_config {
+    machine_type = "e2-medium"
   }
 }
