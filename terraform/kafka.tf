73f4de3ecd90f18dc066c843c8a861503b6b883c
sanskriti-ltc <sanskriti.singh@lloydsbanking.com>
2025-03-18 13:13:37 +0530

    Update kafka.tf

--- terraform/kafka.tf
+++ terraform/kafka.tf
@@ -1,15 +1,42 @@
-resource "helm_release" "kafka" {
-  name       = "kafka"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kafka"
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"  # Update with your Kubernetes config path if needed
+  }
+}
 
+resource "helm_release" "kafka" {
+  name       = "my-kafka"
+  chart      = "bitnami/kafka"
+  version    = "latest"  # You can specify a specific version here, if needed
+  namespace  = "default" # You can specify a namespace here if needed
+  
   set {
     name  = "replicaCount"
-    value = 1
+    value = "1"
   }
 
   set {
-    name  = "service.type"
-    value = "LoadBalancer"
+    name  = "persistence.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = "50m"
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = "64Mi"
+  }
+
+  set {
+    name  = "listeners.client.protocol"
+    value = "PLAINTEXT"
+  }
+
+  set {
+    name  = "autoCreateTopicsEnable"
+    value = "true"
   }
 }
